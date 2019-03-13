fe9f10f09c1d884e12b665bdb24ed1aaa2c3a7b8
JulienMaster21 <julienmaster21@gmail.com>
2019-03-13 09:30:44 +0100

    Add fontawesome to emptyPage template

--- resources/views/templates/emptyPage.blade.php
+++ resources/views/templates/emptyPage.blade.php
@@ -9,6 +9,7 @@
         <link href="{{asset("css/reset.css")}}" type="text/css" rel="stylesheet"></link>
         <link href="{{asset("css/general.css")}}" type="text/css" rel="stylesheet"></link>
         <link href="{{asset("images/admech_logo.ico")}}" type="image/ico" rel="icon"></link>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
         <script src="{{asset("js/general.js")}}" rel="javascript"></script>
         <title>@yield("title")</title>
     </head>
