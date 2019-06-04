50f4882d9049890827f4d1a1e4c74490fb3ec7f6
JulienMaster21 <julienmaster21@gmail.com>
2019-06-04 15:58:11 +0200

    change auth_id to auth_level in registercontroller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,7 +67,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'auth_id' => 1,
+            'auth_level' => 1,
         ]);
     }
 }
