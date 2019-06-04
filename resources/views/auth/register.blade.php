f13c06673e1fa67299d949cd685585c0e785725f
JulienMaster21 <julienmaster21@gmail.com>
2019-06-04 16:13:47 +0200

    Add auth_level field to register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,14 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label for="auth_level" class="col-md-4 col-form-label text-md-right">Authenticatie level</label>
+
+                            <div class="col-md-6">
+                                <input id="auth_level" type="text" class="form-control" name="auth_level" required>
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
