22b83f8b0194905f3034a8776a3f81afe5bdda07
JulienMaster21 <julienmaster21@gmail.com>
2019-06-14 18:58:53 +0200

    Fix rememberToken

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -27,7 +27,7 @@ class CreateUsersTable extends Migration
             $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('remember_token')->default(remember_token());
+            $table->rememberToken();
 
             $table->unique(["id"], 'id_UNIQUE');
 
