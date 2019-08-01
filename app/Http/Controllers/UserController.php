a6b96ab525b0d22d22a1eceb80d6d21e64fc5df5
JulienMaster21 <julienmaster21@gmail.com>
2019-08-01 17:04:34 +0200

    add authorisation to UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,6 +11,17 @@ use App\User;
 class UserController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('verified');
+        $this->authorizeResource(User::class, "user");
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
